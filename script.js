ae49957136d5ff9ebb39acef868b78baf7b7fe4f
jmgamer1199 <40575358+jmgamer1199@users.noreply.github.com>
2023-05-01 17:20:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ window.addEventListener('focus', () => {
 }) */
 
 const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-const slider = document.getElementById('dark-mode-toggle-1')
+const slider = document.getElementById('dark-mode-toggle-1');
 
 const setTheme = (theme) => {
     document.documentElement.setAttribute('root', theme);
-    localStorage.setItem('theme', theme)
+    localStorage.setItem('theme', theme);
 }
 
-slider.addEventListener('toggle', () => {
+slider.addEventListener('click', ()  => {
     let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
     setTheme(switchToTheme);
 });
